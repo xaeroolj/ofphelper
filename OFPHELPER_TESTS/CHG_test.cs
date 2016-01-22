a2f4b97118726f275c090dbfbdb9aba99e5e1715
xaero <xaero_olj@mail.ru>
2016-01-22 13:46:02 +0300

    latest

---
+++ OFPHELPER_TESTS/CHG_test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SITAOFPHelper;
+
+namespace OFPHELPER_TESTS
+{
+    [TestClass]
+    public class CHG_test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
